9ffbb1d08fc828fabec1e9f0034b6acb400ca56a
Marcos Rios <marcosarios@gmail.com>
2020-07-07 09:38:48 -0300

    Initial commit

---
+++ funciones.c
@@ -0,0 +1,70 @@
+/**//* 22.333.444-PEREZ_DEL_RIO,JuanManuel-(07-2299) *//**/
+
+
+/**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
+/**//**//* CUALQUIER INCLUDE DE BIBLIOTECA QUE NECESITE, HÁGALO ACÁ   *//**//**/
+
+/**//**//* CUALQUIER INCLUDE DE BIBLIOTECA QUE NECESITE, HÁGALO ACÁ   *//**//**/
+/**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
+#include "funciones.h"
+/**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
+/**//**//* ACÁ DEBE DESARROLLAR LAS FUNCIONES Y PRIMITIVAS PEDIDAS    *//**//**/
+/**//**//* ADEMÁS DE CUALQUIER OTRA FUNCIÓN QUE SE REQUIERA           *//**//**/
+/**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
+/**//* FUNCIONES Y PRIMITIVAS A DESARROLLAR                               *//**/
+/**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
+/**//* para la información                                                *//**/
+/**
+void mostrarFinal_MIO(const void *d, FILE *fp)
+{
+
+}
+ **/
+/**
+int  compararFinal_MIO(const void *d1, const void *d2)
+{
+
+}
+ **/
+/**
+int  acumularCalif_MIO(void **dest, unsigned *tamDest,
+                       const void *orig, unsigned tamOrig)
+}
+
+}
+ **/
+/**
+void mostrarProm_MIO(const void *d2, int cantCalif, FILE *fp)
+{
+
+}
+ **/
+
+/**//* para el TDA LISTA                                                  *//**/
+/**
+int  mostrarDeIzqADer_MIO(const tLista *p,
+                          void(*mostrar)(const void *, FILE *), FILE *fp)
+{
+
+}
+ **/
+/**
+void ordenarLista_MIO(tLista *p, int (*comparar)(const void *, const void *))
+{
+
+}
+ **/
+/**
+int  vaciarMostrarDetalleYProm_MIO(tLista *p, FILE *fp,
+                                   int (*comparar)(const void *, const void *),
+                                   int (*acumular)(void **, unsigned *,
+                                                   const void *, unsigned),
+                                   void (*mostrar)(const void *, FILE *),
+                                   void (*mostrar2)(const void *, int, FILE *))
+{
+
+}
+ **/
+
+/**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
+
